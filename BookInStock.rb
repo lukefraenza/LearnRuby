6f20aa7a0d2c1b2092bd80cdbd311e7315e1e147
Luke Fraenza <luke.fraenza@gmail.com>
2009-11-05 11:48:57 -0500

    BookInStock.rb: Virtual Attributes (getting and setting)

--- BookInStock.rb
+++ BookInStock.rb
@@ -7,13 +7,21 @@ class BookInStock
     @price = Float(price)
   end
 
+  def price_in_cents
+    Integer(price*100 + 0.5)
+  end
+
+  def price_in_cents=(cents)
+    @price = cents / 100.0  
+  end
+
   def to_s
     "ISBN: #{@isbn}, price: #{@price}"
   end
 
 end
 
-b1 = BookInStock.new("isbn1",3)
+b1 = BookInStock.new("isbn1",33.80)
 puts b1
 b2 = BookInStock.new("isbn2",3.14)
 puts b2
@@ -25,3 +33,6 @@ puts "Price = #{b1.price}"
 
 b1.price = b1.price * 0.75  #discount price
 puts "New price = #{b1.price}"
+puts "Price in cents = #{b1.price_in_cents}"
+b1.price_in_cents = 1234
+puts "price set by cents: #{b1.price}"
