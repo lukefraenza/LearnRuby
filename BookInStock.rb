1005584f4a69ec225092e12479fe971b52d1ea0d
Luke Fraenza <luke.fraenza@gmail.com>
2009-11-05 11:18:58 -0500

    BookInStock.rb: creating instances

--- BookInStock.rb
+++ BookInStock.rb
@@ -7,5 +7,10 @@ class BookInStock
 
 end
 
-a_book = BookInStock.new
-b_book = BookInStock.new
+b1 = BookInStock.new("isbn1",3)
+p b1
+b2 = BookInStock.new("isbn2",3.14)
+p b2
+b3 = BookInStock.new("isbn3","5.67")
+p b3
+
