32ffd5111c2c9cfe752cf309c6a2a22abab1bf4e
Luke Fraenza <luke.fraenza@gmail.com>
2009-11-05 10:30:48 -0500

    control.rb: some control tricks

---
+++ control.rb
@@ -0,0 +1,19 @@
+
+radiation = 100000
+
+if radiation > 3000
+    puts "Danger!"
+end
+
+puts "whoah!" if radiation > 3000
+
+
+square = 2
+while square < 1000
+    square = square*square
+end
+puts square
+
+sq = 2
+sq = sq*sq while sq < 1000
+puts sq
