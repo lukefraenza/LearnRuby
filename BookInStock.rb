b6a67879af18e197bfd7602ccb5d98744f864f1d
Luke Fraenza <luke.fraenza@gmail.com>
2009-11-05 11:20:57 -0500

    BookInStock.rb: added to_s method

--- BookInStock.rb
+++ BookInStock.rb
@@ -5,12 +5,16 @@ class BookInStock
     @price = Float(price)
   end
 
+  def to_s
+    "ISBN: #{@isbn}, price: #{@price}"
+  end
+
 end
 
 b1 = BookInStock.new("isbn1",3)
-p b1
+puts b1
 b2 = BookInStock.new("isbn2",3.14)
-p b2
+puts b2
 b3 = BookInStock.new("isbn3","5.67")
-p b3
+puts b3
 
