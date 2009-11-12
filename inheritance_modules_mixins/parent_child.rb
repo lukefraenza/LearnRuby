5332795d508139d837f8ae3d4c58ca61d9df9b56
Luke Fraenza <luke.fraenza@gmail.com>
2009-11-12 15:01:06 -0500

    parent_child.rb: beginning work with class inheritance

---
+++ inheritance_modules_mixins/parent_child.rb
@@ -0,0 +1,15 @@
+class Parent
+  def say_hello
+    puts "Hello from #{self}"
+  end
+end
+
+p = Parent.new
+p.say_hello
+
+# Subclass the parent...
+class Child < Parent
+end
+
+c = Child.new
+c.say_hello
