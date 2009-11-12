c59f30166a666eadad25389704d0cedc6cac6e78
Luke Fraenza <luke.fraenza@gmail.com>
2009-11-12 14:52:20 -0500

    proc_example.rb: added new Proc syntax

--- containers_blocks_and_iterators/proc_example.rb
+++ containers_blocks_and_iterators/proc_example.rb
@@ -36,3 +36,66 @@ p1.call(3)
 p1.call(4)
 p2 = n_times("Hello ")
 p2.call(3)
+
+
+# another lambda notation
+
+proc1 = -> arg { puts "In proc1 with #{arg}" }
+proc2 = -> arg1, arg2 {  puts "In proc2 with #{arg1} and #{arg2}"  }
+proc3 = ->(arg1, arg2) {  puts "In proc3 with #{arg1} and #{arg2}"  }
+proc1.call "ant"
+proc2.call "bee", "cat"
+proc3.call "dog", "elk"
+
+
+# pass one or more Proc objects to a method
+
+
+def my_if(condition, then_clause, else_clause)
+  if condition
+    then_clause.call
+  else
+    else_clause.call
+  end
+end
+
+5.times do |val|
+  my_if val < 3,
+    -> { puts "#{val} is small " },
+    -> { puts "#{val} is big " }
+end
+
+
+# redoing a while loop
+
+def my_while(cond, &body)
+  while cond.call
+    body.call
+  end
+end
+
+a = 0
+my_while -> { a < 3 } do
+  puts a
+  a += 1
+end
+
+
+# block Parameter lists
+proc1 = lambda do |a, *b, &block|
+  puts "a = #{a.inspect} "
+  puts "b = #{b.inspect} "
+  block.call
+end
+proc1.call(1,2,3,4) { puts "in block1" }
+
+proc2 = -> a, *b, &block do
+  puts "a = #{a.inspect} "
+  puts "b = #{b.inspect} "
+  block.call
+end
+proc2.call(1,2,3,4) { puts "in block2" }
+
+
+
+
