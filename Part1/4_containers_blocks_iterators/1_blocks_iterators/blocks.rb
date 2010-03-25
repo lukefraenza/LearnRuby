4a06e97856c20da457a930e4db3f8226dff70c3a
Luke Fraenza <luke.fraenza@gmail.com>
2010-03-24 22:50:53 -0400

    working with blocks...

--- Part1/4_containers_blocks_iterators/1_blocks_iterators/blocks.rb
+++ Part1/4_containers_blocks_iterators/1_blocks_iterators/blocks.rb
@@ -1,13 +1,21 @@
 # block: chunk of code
 
-some_array = %w( all you single ladies)
-p some_array
+# some_array = %w( all you single ladies)
+# p some_array
 
-some_array.each {|value| puts value * 3 }
+# some_array.each {|value| puts value * 3 }
+# sum = 0
+
+# other_array = [1,2,3,4]
+# other_array.each do |value|
+#   sum += value
+#   puts value / sum
+#end
+
+# sum the squares of numbers in an array:
 sum = 0
-
-other_array = [1,2,3,4]
-other_array.each do |value|
-  sum += value
-  puts value / sum
+[1, 2, 3, 4].each do |value|
+  square = value * value
+  sum   += square
 end
+puts sum
