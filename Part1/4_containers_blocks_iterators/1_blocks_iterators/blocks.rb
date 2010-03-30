a5df9d0ace49e6cb2753f76ca51af85ed468a449
Luke Fraenza <luke.fraenza@gmail.com>
2010-03-30 19:07:03 -0400

    learning about iterators

--- Part1/4_containers_blocks_iterators/1_blocks_iterators/blocks.rb
+++ Part1/4_containers_blocks_iterators/1_blocks_iterators/blocks.rb
@@ -19,3 +19,19 @@ sum = 0
   sum   += square
 end
 puts sum
+
+# parameters to a block are *always* local to a block, even if they 
+# have the same name as locals in teh surrounding scope.
+
+value = "some shape"
+[1,2].each {|value| puts value}
+puts value
+
+square = "some shape"
+sum = 0
+[1,2,3,4].each do |value; square| # square is now block-local
+  square = value * value
+  sum   += square
+end
+puts sum
+puts square
---
+++ Part1/4_containers_blocks_iterators/1_blocks_iterators/iterators.rb
@@ -0,0 +1,61 @@
+# An iterator is a method that can invoke a block of code
+
+def three_times
+  yield
+  yield
+  yield
+end
+
+three_times { puts "Hello"}
+
+def fib_up_to(max)
+  i1, i2, = 1, 1      # parallel assignment (i1 = 1 and i2 = 1)
+  while i1 <= max
+    yield i1
+    i1, i2 = i2, i1+i2
+  end
+end
+fib_up_to(1) {|f| print f, " " }
+puts " "
+fib_up_to(1000) {|f| print f, " " }
+puts " "
+
+# the value of the last expression evaluated in the block is 
+# passed back to the method as the value of the yield
+
+class Array
+  def find
+    for i in 0...size
+      value = self[i]
+      return value if yield(value)
+    end
+    return nil
+  end
+end
+
+puts [1,3,5,7,9].find {|v| v*v > 30 }
+
+# each yields successive elements of its collection
+[1,3,5,7,9].each {|i| puts i}
+
+# collect (aka map) takes each element from the collection and
+# passes it to the block.  The results returned by the block are
+# used to construct a new array.
+# The following uses the succ method, which increments a string value:
+
+array1 = ["H", "A", "L"].collect {|x| x.succ }
+p array1
+
+f = File.open("testfile")
+f.each do |line|
+  puts "The line is: #{line}"
+end
+f.close
+
+f = File.open("testfile")
+f.each_with_index do |line,  index|
+  puts "Line #{index} is: #{line}"
+end
+f.close
+
+
